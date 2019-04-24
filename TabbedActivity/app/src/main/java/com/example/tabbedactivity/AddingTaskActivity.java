e56e2f00561ffbbd0c97051956a28ea29d64dda2
abhilash0717 <40905548+abhilash0717@users.noreply.github.com>
2019-04-24 18:24:33 +0530

    first commit

---
+++ TabbedActivity/app/src/main/java/com/example/tabbedactivity/AddingTaskActivity.java
@@ -0,0 +1,94 @@
+package com.example.tabbedactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddingTaskActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    DatabaseHelper mDatabaseHelper;
+    private Button btnAdd, btnViewData;
+    private EditText editText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding_task);
+
+        if(getSupportActionBar() != null){
+
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        getSupportActionBar().setTitle("Calender");
+
+        editText = (EditText) findViewById(R.id.TaskEditText);
+        btnAdd = (Button) findViewById(R.id.buttonSave);
+        btnViewData = (Button) findViewById(R.id.buttonContents);
+        mDatabaseHelper = new DatabaseHelper(this);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = editText.getText().toString();
+                if (editText.length() != 0) {
+                    AddData(newEntry);
+                    editText.setText("");
+                } else {
+                    toastMessage("You must put something in the text field!");
+                }
+
+            }
+        });
+
+        btnViewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddingTaskActivity.this, ListDataActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void AddData(String newEntry) {
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+
+        if (insertData) {
+            toastMessage("Data Successfully Inserted!");
+        } else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home);
+        finish();
+        return super.onOptionsItemSelected(item);
+
+
+        }
+    }
+
+
+
+
+
